6f5a5da8d138648dbae15e15fe6dd34365d51216
Chico <francisco.ramos.1997@gmail.com>
2023-09-26 23:59:13 +0100

    obrigado bro git commit obrigado bro git diff

---
+++ aula1/code1/python1.py
@@ -0,0 +1,11 @@
+a = 0
+
+while True:
+    print(a)
+    if a == 10:
+        break 
+    a = a + 1
+
+
+    
+    
---
+++ aula1/code1/python2.py
@@ -0,0 +1,6 @@
+a = 0
+
+if a % 2 == 0:
+    print("par")
+if a % 2 != 0:
+    print("impar")
