36819d70d629914dc1ea3c08f16dfe3a77bdd88e
unknown <miguelacopt@gmail.com>
2023-09-30 11:53:01 +0100

    Added exercises

---
+++ aula1/code1/exercise1.py
@@ -0,0 +1,37 @@
+
+
+# Vou usar ingles para te habituares a ver os termos em ingles para procurar no google
+
+# 1 - Create a script that will accept two integers and tell me which ones is greater than the other
+
+int1 = 1
+int2 = 2
+# Your code here
+
+# 2 - Create a script that will accept two strings, comebine them and print them out together
+
+string1 = "Hello,"
+string2 = "World!"
+# Your code here
+
+# 3 - Create a script that will accept two integers and tell me the difference between the two.
+# Example 3 and 5 the result would be 2
+
+number1 = 5
+number2 = 12
+# Your code here
+
+# 4 - Create a script that accepts a number and it will count from 0 to that number
+
+countTo = 20
+# Your code here
+
+# 5 - Create a script that accepts a string and tells me how many vowels it has.
+
+vowels = "This is a test string for test purposes"
+# Your code here
+
+# 6 - Create a script that accepts an integer rand tells me if that integer is prime.
+
+prime = 13
+# Your code here
